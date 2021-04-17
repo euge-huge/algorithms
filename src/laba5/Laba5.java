f21cc5be122d3ac1d5c43a90031ea45293ae2824
Eugene Bystrov <evgeniy0ctreles@gmail.com>
2021-04-17 17:45:39 +0700

    Закончена лаба 5

--- src/laba5/Laba5.java
+++ src/laba5/Laba5.java
@@ -1,2 +1,36 @@
-package laba5;public class Laba5 {
+package laba5;
+
+import laba5.stack.Stack;
+
+import java.io.*;
+
+public class Laba5 {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stackOfLength = new Stack<>();
+        Stack<Integer> stackOfWordsCount = new Stack<>();
+        Stack<String> stackOfLines = new Stack<>();
+
+        FileInputStream _file = new FileInputStream("D:\\projects\\algorithms\\src\\laba5\\file.txt");
+        InputStreamReader file = new InputStreamReader(_file, "UTF-8");
+
+        // Считываем файл в одну большую строку
+        int i;
+        StringBuilder bigLine = new StringBuilder();
+        while ((i = file.read()) != -1) {
+            bigLine.append((char) i);
+        }
+
+        for (String line : bigLine.toString().split(System.lineSeparator())) {
+            stackOfLength.push(line.length());
+            stackOfWordsCount.push(line.split(" ").length);
+            stackOfLines.push(line);
+        }
+
+        int countOfLines = stackOfLines.size();
+        for (int j = 0; j < countOfLines; j++) {
+            System.out.println(stackOfLines.pop() + "\t" + stackOfLength.pop() + "\t" + stackOfWordsCount.pop());
+        }
+
+        file.close();
+    }
 }
---
+++ src/laba5/stack/Stack.java
@@ -0,0 +1,38 @@
+package laba5.stack;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+    private ArrayList<T> list = new ArrayList<>();
+    public int top;
+
+    public Stack() {
+        ArrayList<T> stackArray = new ArrayList<>();
+        this.top = -1;
+    }
+
+    public void push(T object) {
+        list.add(0, object);
+    }
+
+    public T pop() {
+        return list.remove(0);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public T top() {
+        if (!list.isEmpty()) {
+            return list.get(0);
+        } else {
+            throw new EmptyStackException();
+        }
+    }
+}
